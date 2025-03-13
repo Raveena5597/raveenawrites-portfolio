91c84c93e9df71a40076ed2c2b647b67ce1fcd1f
Raveena <raveena.putta@gmail.com>
2025-03-13 13:13:45 +0530

    create ticket dynamically

--- projects/KanbanBoard/script.js
+++ projects/KanbanBoard/script.js
@@ -12,13 +12,42 @@ addBtn.addEventListener('click', function () {
     } else {
         addTaskFlag = true;
     }
-
     console.log('add btn clicked')
-
     if (addTaskFlag) {
         modalCont.style.display = 'flex'
     } else {
         modalCont.style.display = 'none'
     }
+
+})
+
+// create a ticket dynamically.
+
+function createTicket() {
+    //create a new ticket HTML (container element)
+    const ticketCont = document.createElement('div');
+    ticketCont.classList.add('ticket-cont');
+    // OR
+    // ticketCont.setAttribute('class', 'ticket-cont')
+    ticketCont.innerHTML = `
+        <div class="ticket-color"></div>
+        <div class="ticket-id">123</div>
+        <div class="ticket-area">Study today.</div>
+        <div class="ticket-lock">
+            <i class="fa-solid fa-lock"></i>
+            <!-- <i class="fa-solid fa-lock-open"></i> -->
+        </div>
+    `;
+    const mainCont = document.querySelector('.main-cont');
+    mainCont.appendChild(ticketCont);
+}
+
+modalCont.addEventListener('keydown', function (ev) {
+    if (ev.key === 'Shift') {
+        createTicket()
+        modalCont.style.display = 'none'; // hide the modal.
+        textAreaCont.value = ''; // clear contents on close.
+    };
+
 
 })
